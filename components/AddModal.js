867b8b0e2d8fac5790a79843f8fc1e914040ff87
tami <xorud7979@gmail.com>
2022-10-05 10:52:13 +0900

    feat: date-picker component 추가

--- components/AddModal.js
+++ components/AddModal.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { useForm } from "react-hook-form";
+import BasicDatePicker from "./DatePicker";
 
 const style = {
   position: "absolute",
@@ -17,12 +20,19 @@ const style = {
 };
 
 const AddModal = () => {
+  //react-hook-form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
   const [open, setOpen] = useState();
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen}>가맹점 추가</Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -31,13 +41,72 @@ const AddModal = () => {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            가맹점 등록
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <form onSubmit={handleSubmit()}>
+              <label>가맹점명</label>
+              <input
+                {...register("name", { required: "필수 입력값 입니다." })}
+                placeholder="상호명을 입력해주세요."
+              />
+              {errors.name && <p>{errors.name.message}</p>}
+              {/* {<p>{errors.name}</p>} */}
+              <br />
+              <label>업종</label>
+              <input
+                {...register("category", { required: "필수 입력값 입니다." })}
+                placeholder="업종을 입력해주세요."
+              />
+              {<p>{errors.category}</p>}
+              <br />
+              <label>등록일자</label>
+              <BasicDatePicker />
+              {/* date picker 사용*/}
+              <br />
+              <label>사업규모</label>
+              <select {...register("scale")}>
+                <option value="small">영세</option>
+                <option value="medium1">중소1</option>
+                <option value="medium2">중소2</option>
+                <option value="medium3">중소3</option>
+                <option value="self">일반</option>
+              </select>
+              <br />
+              <label>수수료율</label>
+              <input
+                {...register("fee", {
+                  required: "필수 입력값 입니다.",
+                  valueAsNumber: true,
+                  validate: (value) => value > 0,
+                })}
+              />
+              {<p>{errors.fee}</p>}
+              % <br />
+              <button type="submit">등록하기</button>
+            </form>
           </Typography>
         </Box>
       </Modal>
+      <style>{`
+        .css-1wnsr1i {
+          width: 500px;
+          height: 550px;
+          border: none;
+          border-radius: 20px;
+        }
+        label {
+          display: inline-block;
+          width: 100px;
+          height: 60px;
+        }
+        input {
+          width: 300px;
+          height: 30px;
+          background: #BAD7DF;
+          border: none;
+        }
+        `}</style>
     </div>
   );
 };
