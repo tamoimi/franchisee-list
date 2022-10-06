f104bf5f31d0081eb750d4740a7c835de6d7b26b
tami <xorud7979@gmail.com>
2022-10-06 17:59:30 +0900

    feat: get(pagination)추가

--- pages/api/franchisees/index.js
+++ pages/api/franchisees/index.js
@@ -3,22 +3,23 @@ import client from "../../libs/prismaClient,js";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     console.log("post 호출!");
-
+    console.log(req.body);
     const {
-      body: {
-        data: { name, category, registDate, scale, fee },
-      },
+      body: { name, category, registDate, scale, fee },
     } = req;
 
     console.log("백엔드 로그:", name, category, registDate, scale, fee);
+
+    // const parsedDate = new Date(registDate);
+    // console.log(parsedDate);
 
     const result = await client.franchisee.create({
       data: {
         name,
         category,
-        registDate,
+        registDate: registDate,
         scale,
-        fee: parseFloat(fee),
+        fee: +fee,
       },
     });
     res.status(200).json(result);
@@ -27,13 +28,27 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     console.log("get 호출!");
 
-    const result = await client.franchisee.findMany({
-      select: {
-        id: true,
-        category: true,
-        scale: true,
-        fee: true,
+    const { currentPage, rowsPerPage } = req.query;
+
+    const skipNumber = currentPage * 10;
+    const take = +rowsPerPage;
+    const result = await client.franchisee.findMany({ skip: skipNumber, take });
+    const totalCount = await client.franchisee.count();
+
+    res.status(200).json({ result: result, totalCount: totalCount });
+  }
+
+  if (req.method === "DELETE") {
+    console.log("delete 호출!");
+    const {
+      body: { id },
+    } = req;
+
+    const result = await client.franchisee.delete({
+      where: {
+        id: +id,
       },
     });
+    res.status(200).json(result);
   }
 }
