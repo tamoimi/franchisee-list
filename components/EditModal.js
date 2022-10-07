c8cf3839fb3958d0ed30a971f397b6667fbc4aa4
tami <xorud7979@gmail.com>
2022-10-07 09:02:19 +0900

    feat: react-query _app.js에 추가

---
+++ components/EditModal.js
@@ -0,0 +1,8 @@
+const EditModal = () => {
+
+
+    
+
+
+}
+export default EditModal
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
