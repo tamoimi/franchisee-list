bc46d91c9ae347ca4bd791b94618edcc27ad9a5e
tami <xorud7979@gmail.com>
2022-10-07 09:01:41 +0900

    feat: 오류 수정

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-export default function BasicDatePicker() {
-    const [value, setValue] = useState();
-  
-    return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label="시작 날짜"
-          inputFormat="yyyy-MM-dd"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
-    );
-  }
-  
+export default function BasicDatePicker({ selectedDate, setSelectedDate }) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label="등록 날짜"
+        inputFormat="YYYY-MM-DD"
+        value={selectedDate}
+        onChange={(value) => {
+          setSelectedDate(value);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+    
+  );
+}
