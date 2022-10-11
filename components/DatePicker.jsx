c9584bd1159b308065411111513e10dd85c0c1fe
tami <xorud7979@gmail.com>
2022-10-11 13:51:19 +0900

    feat: dayjs import

--- components/DatePicker.jsx
+++ components/DatePicker.jsx
@@ -3,8 +3,12 @@ import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+
 
 export default function BasicDatePicker({ selectedDate, setSelectedDate }) {
+  // const dateFormat = dayjs(date).format("YYYY-MM-DD")
+  
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
